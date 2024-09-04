15eacd1958cedbf7d843c52c2e370caa96ef0595
wassefchebbi <chebbimedwassef@gmail.com>
2024-09-04 11:30:34 +0100

    added contact form

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-var express=require('express');
-var app=express();
+var express = require("express");
+var app = express();
 
-var routes=require('./routes/route.js');
+var routes = require("./routes/route.js");
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
+var bodyParser = require("body-parser"); // Add this line
 
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',routes.home);
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", routes.home);
+
+app.get("/contact", routes.contact);
+app.post("/contact", routes.submitContact);
 
 var port = process.env.PORT || 3000;
 
-var server=app.listen(port,function(req,res){
-    console.log("Catch the action at http://localhost:"+port);
+var server = app.listen(port, function (req, res) {
+  console.log("Catch the action at http://localhost:" + port);
 });
